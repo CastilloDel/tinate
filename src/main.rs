47af37d3964f5b9e670a905846de24693dc70840
CastilloDel <delcastillodelarosadaniel@gmail.com>
2020-08-14 10:56:15 +0100

    Print the correct ammount of tildes
    Now the program knows the size of the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::fmt::Write as fmt_write;
 use std::io;
 use std::io::prelude::*;
+use std::io::Write;
 use termios::{
     os::target::TCSAFLUSH, tcsetattr, Termios, ECHO, ICANON, ICRNL, IEXTEN, ISIG, IXON, OPOST,
 };
@@ -11,8 +13,9 @@ fn main() -> Result<(), io::Error> {
     prepare_term_config();
     let _will_get_dropped = AtExit { original_config };
 
+    let (_n_cols, n_rows) = get_term_size();
     loop {
-        refresh_screen();
+        refresh_screen(n_rows);
         if let None = process_key() {
             break Ok(());
         }
@@ -49,10 +52,12 @@ fn read_key() -> u8 {
     }
 }
 
-fn refresh_screen() {
+fn refresh_screen(n_rows: usize) {
     clear_screen();
 
-    draw_rows();
+    if let Err(_) = draw_rows(n_rows) {
+        die("Couldn't refresh the screen");
+    }
 
     print!("\x1b[H");
     io::stdout().flush().expect("Couldn't flush the stdout");
@@ -64,9 +69,21 @@ fn clear_screen() {
     io::stdout().flush().expect("Couldn't flush the stdout");
 }
 
-fn draw_rows() {
-    for _i in 0..24 {
-        println!("~\r");
+fn draw_rows(n_rows: usize) -> std::fmt::Result {
+    let mut s = String::new();
+    for _i in 0..n_rows - 1 {
+        write!(&mut s, "~\r\n")?;
+    }
+    write!(&mut s, "~")?;
+    print!("{}", s);
+    Ok(())
+}
+
+fn get_term_size() -> (usize, usize) {
+    if let Some((w, h)) = term_size::dimensions() {
+        return (w, h);
+    } else {
+        die("Unable to get term size")
     }
 }
 
