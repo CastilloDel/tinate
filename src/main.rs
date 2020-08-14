350275b9f70292665e2a573b4e3f9e4a4a2142b2
CastilloDel <delcastillodelarosadaniel@gmail.com>
2020-08-14 20:33:00 +0100

    Implement cursor movement

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,11 @@ use std::io;
 use std::io::prelude::*;
 use std::io::Write;
 use std::process::exit;
+use std::sync::atomic::{AtomicU16, Ordering::Relaxed};
 
 const WELCOME_MESSAGE: &'static str = "Tinate Is Not A Text Editor";
+static X: AtomicU16 = AtomicU16::new(0);
+static Y: AtomicU16 = AtomicU16::new(0);
 
 fn main() -> Result<()> {
     execute!(io::stdout(), EnterAlternateScreen)?;
@@ -46,6 +49,7 @@ fn process_key() -> Result<()> {
             disable_raw_mode()?;
             exit(0);
         }
+        key if is_movement_key(key) => move_cursor(key),
         _ => Ok(()),
     }
 }
@@ -61,7 +65,7 @@ fn refresh_screen() -> Result<()> {
 
     draw_rows()?;
 
-    queue!(io::stdout(), MoveTo(0, 0))?;
+    queue!(io::stdout(), MoveTo(X.load(Relaxed), Y.load(Relaxed)))?;
     io::stdout().flush()?;
     Ok(())
 }
@@ -106,6 +110,28 @@ fn write_padding(s: &mut String, n_cols: u16) -> std::fmt::Result {
     Ok(())
 }
 
+fn move_cursor(key: u8) -> Result<()> {
+    let (n_cols, n_rows) = term_size()?;
+    let x = X.load(Relaxed);
+    let y = Y.load(Relaxed);
+    match key {
+        key if key == 'h' as u8 => X.store(if x > 0 { x - 1 } else { 0 }, Relaxed),
+        key if key == 'j' as u8 => {
+            Y.store(if y < (n_rows - 1) { y + 1 } else { n_rows - 1 }, Relaxed);
+        }
+        key if key == 'k' as u8 => Y.store(if y > 0 { y - 1 } else { 0 }, Relaxed),
+        key if key == 'l' as u8 => {
+            X.store(if x < (n_cols - 1) { x + 1 } else { n_cols - 1 }, Relaxed)
+        }
+        _ => {}
+    }
+    Ok(())
+}
+
 fn control_key(key: u8) -> u8 {
     key & 0x1f
+}
+
+fn is_movement_key(key: u8) -> bool {
+    key == 'h' as u8 || key == 'j' as u8 || key == 'k' as u8 || key == 'l' as u8
 }
