6640c041aa0534c441f05cbea6d0a14c49598ce2
CastilloDel <delcastillodelarosadaniel@gmail.com>
2020-08-16 13:35:25 +0100

    Show the contents of the file passed as a cli arg
    This is a big commit and it is the first time tinate
    reads from a file. You can't still change anything or even
    look more than the first "page" of the file, but it is a
    good step

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ use crossterm::{
     },
     Result,
 };
+use std::env;
 use std::fmt::Write as fmt_write;
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::process::exit;
@@ -23,8 +25,14 @@ fn main() -> Result<()> {
     enable_raw_mode()?;
     let _will_get_dropped = AtExit {};
 
+    let args: Vec<String> = env::args().collect();
+    let mut buffer: Vec<String> = Vec::new();
+    if args.len() >= 2 {
+        buffer = load_to_buf(&args[1]).expect("Invalid path or file");
+    }
+
     loop {
-        refresh_screen()?;
+        refresh_screen(&buffer)?;
         process_key()?;
     }
 }
@@ -36,6 +44,15 @@ impl Drop for AtExit {
         //The result isn't managed because it could cause a panic during a panic
         disable_raw_mode().ok();
     }
+}
+
+fn load_to_buf(path: &str) -> io::Result<Vec<String>> {
+    let file = File::open(path)?;
+    let buffer: Vec<String> = io::BufReader::new(file)
+        .lines()
+        .map(|line_result| line_result.map(|line| line.trim_end().to_string()))
+        .collect::<io::Result<Vec<String>>>()?;
+    Ok(buffer)
 }
 
 fn process_key() -> Result<()> {
@@ -59,34 +76,45 @@ fn read_key() -> Result<u8> {
     Ok(key[0])
 }
 
-fn refresh_screen() -> Result<()> {
+fn refresh_screen(buf: &Vec<String>) -> Result<()> {
     queue!(io::stdout(), MoveTo(0, 0))?;
 
-    draw_rows()?;
+    draw_rows(buf)?;
 
     queue!(io::stdout(), MoveTo(X.load(Relaxed), Y.load(Relaxed)))?;
     io::stdout().flush()?;
     Ok(())
 }
 
-fn draw_rows() -> Result<()> {
+fn draw_rows(buf: &Vec<String>) -> Result<()> {
     let mut s = String::new();
     let (n_cols, n_rows) = term_size()?;
+    let (n_cols, n_rows) = (n_cols as usize, n_rows as usize);
     for i in 0..n_rows {
-        queue!(io::stdout(), Clear(ClearType::CurrentLine))?;
-        if i == n_rows / 3 {
-            add_welcome_message(&mut s, n_cols)?;
-        } else if i == n_rows - 1 {
-            write!(&mut s, "~")?;
+        queue!(s, Clear(ClearType::CurrentLine))?;
+        if i < buf.len() {
+            let mut truncated_line = buf[i].clone();
+            truncated_line.truncate(n_cols);
+            if i == n_rows - 1 {
+                write!(&mut s, "{}\r", &truncated_line)?;
+            } else {
+                write!(&mut s, "{}\r\n", &truncated_line)?;
+            }
         } else {
-            write!(&mut s, "~\r\n")?;
+            if buf.len() == 0 && i == n_rows / 3 {
+                add_welcome_message(&mut s, n_cols)?;
+            } else if i == n_rows - 1 {
+                write!(&mut s, "~")?;
+            } else {
+                write!(&mut s, "~\r\n")?;
+            }
         }
     }
     print!("{}", s);
     Ok(())
 }
 
-fn add_welcome_message(s: &mut String, n_cols: u16) -> std::fmt::Result {
+fn add_welcome_message(s: &mut String, n_cols: usize) -> std::fmt::Result {
     let mut msg = String::from(WELCOME_MESSAGE);
     if WELCOME_MESSAGE.len() > n_cols as usize {
         msg.truncate(n_cols as usize);
@@ -96,7 +124,7 @@ fn add_welcome_message(s: &mut String, n_cols: u16) -> std::fmt::Result {
     write!(s, "{}\r\n", msg)
 }
 
-fn write_padding(s: &mut String, n_cols: u16) -> std::fmt::Result {
+fn write_padding(s: &mut String, n_cols: usize) -> std::fmt::Result {
     let padding = (n_cols as usize - WELCOME_MESSAGE.len()) / 2;
     if padding > 0 {
         write!(s, "~")?;
